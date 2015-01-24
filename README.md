66ff81bce5fdb62593e1695b71eb4beecda58eb7
Mike Peutz <mpeutz@gmail.com>
2015-01-24 14:02:17 -0800

    Moved local files to git

---
+++ README.md
@@ -0,0 +1,23 @@
+# gulp-msg
+
+Gulp plugin for MSG (Magnificent styleguide) an enhanced version of KSS ([Knyle Stype Sheets](http://warpspire.com/kss/)) documentation generation.
+
+This plugin is based on [kss-node](https://github.com/hughsk/kss-node) and generates a styleguide based on code documentation. The plugin is mainly a fork of `kss-nodes`'s bin script.
+
+This plugin currently lacks tests.
+
+## Install
+
+```
+npm install gulp-msg
+```
+
+## Usage
+
+Documentation to come
+
+## Options
+
+* `overview`: Absolute path to markdown file which is used for styleguide home page
+* `templateDirectory`: Absolute path to template directory, by default `kss-node` default template is used.
+* `msg`: Options supported by [`msg-node`](https://github.com/mpeutz/msg)
---
+++ index.js
@@ -0,0 +1,181 @@
+var path = require('path');
+var fs = require('fs');
+
+var through = require('through');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var gulpless = require('gulp-less');
+var msg = require('msg');
+var marked = require('marked');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+
+
+/*
+    This script is based and recycles a lot of code of the bin script of kss-node
+    https://github.com/hughsk/kss-node/blob/master/bin/kss-node
+ */
+module.exports = stringify;
+
+function getSerialize (fn, decycle) {
+  var seen = [], keys = [];
+  decycle = decycle || function(key, value) {
+    return '[Circular ' + getPath(value, seen, keys) + ']'
+  };
+  return function(key, value) {
+    var ret = value;
+    if (typeof value === 'object' && value) {
+      if (seen.indexOf(value) !== -1)
+        ret = decycle(key, value);
+      else {
+        seen.push(value);
+        keys.push(key);
+      }
+    }
+    if (fn) ret = fn(key, ret);
+    return ret;
+  }
+}
+
+function getPath (value, seen, keys) {
+  var index = seen.indexOf(value);
+  var path = [ keys[index] ];
+  for (index--; index >= 0; index--) {
+    if (seen[index][ path[0] ] === value) {
+      value = seen[index];
+      path.unshift(keys[index]);
+    }
+  }
+  return '~' + path.join('.');
+}
+
+function stringify(obj, fn, spaces, decycle) {
+  return JSON.stringify(obj, getSerialize(fn, decycle), spaces);
+}
+
+stringify.getSerialize = getSerialize;
+
+module.exports = function(opt) {
+    'use strict';
+    if (!opt) opt = {};
+    if (!opt.templateDirectory) opt.templateDirectory = __dirname + '/node_modules/msg/lib/template';
+    if (!opt.msgOpts) opt.msgOpts = {};
+
+    var buffer = [];
+    var firstFile = null;
+
+    /* Is called for each file and writes all files to buffer */
+    function bufferContents(file){
+        if (file.isNull()) return; // ignore
+        if (file.isStream()) return this.emit('error', new PluginError('gulp-msg',  'Streaming not supported'));
+
+        if (!firstFile) firstFile = file;
+
+        buffer.push(file.contents.toString('utf8'));
+    }
+
+    /* Is called when all files were added to buffer */
+    function endStream(){
+
+        var self = this;
+
+        msg.parse(buffer, opt.msgOpts, function (err, styleguide) {
+            if (err) console.log('Error', error);
+
+                var sections = styleguide.section('*.'),
+                    k, j, i, sectionCount = sections.length,
+                    sectionRoots = [], currentRoot,
+                    rootCount, childSections = [];
+
+
+            // Accumulate all of the sections' first indexes
+            // in case they don't have a root element.
+            for (i = 0; i < sectionCount; i += 1) {
+                currentRoot = sections[i].reference().match(/[0-9]*\.?/)[0].replace('.', '');
+
+                if (!~sectionRoots.indexOf(currentRoot)) {
+                    sectionRoots.push(currentRoot);
+                }
+            }
+
+
+            sectionRoots.sort();
+            rootCount = sectionRoots.length;
+
+
+            // Output msg results as a json file
+            gulp.src(opt.templateDirectory)
+                .pipe(through(function (file) {
+
+                    var content = stringify(sections, null, 2);
+
+                    var joinedPath = path.join(firstFile.base, '/public/view/section.json');
+
+                    var file = new File({
+                        cwd: firstFile.cwd,
+                        base: firstFile.base,
+                        path: joinedPath,
+                        contents: new Buffer(content)
+                    });
+
+                    self.emit('data', file);
+                }));
+
+
+            // Copy template assets, sass compilation added because default template uses it
+            gulp.src(path.join(opt.templateDirectory, '/**/*.css'))
+                .pipe(through(function (file) {
+
+                self.emit('data', file);
+            }));
+
+            gulp.src(path.join(opt.templateDirectory, '/*.html'))
+                .pipe(through(function (file) {
+
+                self.emit('data', file);
+            }));
+
+            gulp.src(path.join(opt.templateDirectory, '/**/*.html'))
+                .pipe(through(function (file) {
+
+                self.emit('data', file);
+            }));
+
+            gulp.src(path.join(opt.templateDirectory, '/**/*.js'))
+                .pipe(through(function (file) {
+
+                self.emit('data', file);
+            }));
+
+        });
+
+    }
+
+    function jsonSections(sections) {
+        return sections.map(function(section) {
+            return {
+                header: section.header(),
+                description: section.description(),
+                reference: section.reference(),
+                group: section.group(),
+                depth: section.data.refDepth,
+                deprecated: section.deprecated(),
+                experimental: section.experimental(),
+                modifiers: jsonModifiers(section.modifiers())
+            };
+        });
+    }
+
+    // Convert an array of `MsgModifier` instances to a JSON object.
+    function jsonModifiers (modifiers) {
+        return modifiers.map(function(modifier) {
+            return {
+                name: modifier.name(),
+                description: modifier.description(),
+                className: modifier.className()
+            };
+        });
+    }
+
+    return through(bufferContents, endStream);
+};
